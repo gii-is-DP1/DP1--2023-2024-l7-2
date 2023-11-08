923d851b5666a28b2dafae397f5b04b509a10a84
MarZamZap <marzamzap@alum.us.es>
2023-11-08 20:28:08 +0100

    Changes in cards and especial cards

--- src/main/java/org/springframework/samples/petclinic/card/Card.java
+++ src/main/java/org/springframework/samples/petclinic/card/Card.java
@@ -3,6 +3,8 @@ package org.springframework.samples.petclinic.card;
 import org.springframework.samples.petclinic.model.NamedEntity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -15,13 +17,24 @@ import lombok.Setter;
 public class Card extends NamedEntity {
     @NotBlank
     private String description;
+    @NotBlank
     private Integer position;
     private String badgeImage;
+
+    @Enumerated(EnumType.STRING)
+    private String cardType;
 
     private Integer totalIron;
     private Integer totalGold;
     private Integer totalSteal;
     private Integer totalMedals;
-    private Boolean helpCard;
+
+    public enum cardType {
+        HelpCard,
+        OrcCard,
+        ObjectCard,
+        Other
+    }
+    
 
 }
