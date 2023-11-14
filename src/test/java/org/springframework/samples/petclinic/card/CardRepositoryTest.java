8db58f04c0b7da2a83bf17d341ae3b357f8fe1f9
Alejandro Gallego Llamas <145361530+alegallla@users.noreply.github.com>
2023-11-14 11:27:38 +0100

    CardTest funcionando

--- src/test/java/org/springframework/samples/petclinic/card/CardRepositoryTest.java
+++ src/test/java/org/springframework/samples/petclinic/card/CardRepositoryTest.java
@@ -1,39 +1,38 @@
 package org.springframework.samples.petclinic.card;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.test.annotation.DirtiesContext;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CardRepositoryTest {
 
+    @Autowired
+    private CardRepository cardRepository;
 
-   @Autowired
-   private CardRepository cardRepository;
+    @Test
+    public void testFindAll() {
+        List<Card> cards = cardRepository.findAll();
+        assertEquals(54,cards.size());
+    }
 
-   @Test
-   public void testFindAll() {
-       Card card = new Card();
-       card.setName("Test Card");
-       card.setDescription("sample description");
-       card.setPosition(2);
-       cardRepository.save(card);
-       List<Card> cards = cardRepository.findAll();
-       assertThat(cards.size()).isGreaterThan(0);
-   }
+    @Test
+    public void testFindByName() {
+        Card existingCard = cardRepository.findAll().get(0);
+        String cardName = existingCard.getName();
 
-   @Test
-   public void testFindByName() {
-       List<Card> foundCard = cardRepository.findByName("Alloy Steel");
-       assertThat(foundCard.size()).isGreaterThan(0);
-   }
+        List<Card> cards = cardRepository.findByName(cardName);
+
+        assertEquals(10, cards.size()); //10 cartas de Iron Seam
+        assertEquals(cardName, cards.get(0).getName());
+      
+    }
+
+   
 }
