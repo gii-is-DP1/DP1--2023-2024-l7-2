26b9cb63a99750c5558312545d409b4c57c78b3e
DelfinSR <delsanrub@alum.us.es>
2024-01-02 22:38:48 +0100

    Working on open matches

--- src/main/java/org/springframework/samples/dwarf/player/Player.java
+++ src/main/java/org/springframework/samples/dwarf/player/Player.java
@@ -8,15 +8,11 @@ import java.util.List;
 import org.springframework.samples.dwarf.game.Game;
 import org.springframework.samples.dwarf.model.NamedEntity;
 import org.springframework.samples.dwarf.object.Object;
-import org.springframework.samples.dwarf.player.gameDeserializer;
-import org.springframework.samples.dwarf.player.gameSerializer;
 import org.springframework.samples.dwarf.user.User;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
@@ -24,7 +20,6 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(name = "Player")
 public class Player extends NamedEntity {
     @NotBlank
     private String color;
@@ -32,6 +27,8 @@ public class Player extends NamedEntity {
     private Integer gold;
     private Integer iron;
     private Integer medal;
+
+    private Boolean isEspectator;
 
     @ManyToOne(optional = true)
     private User user;
