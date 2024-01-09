ca31e92363a817a73dc25ed2334fee9400e02396
Alejandro Gallego Llamas <145361530+alegallla@users.noreply.github.com>
2024-01-09 18:55:06 +0100

    lista jugadores en public games

--- frontend/src/game/gamesPublics.js
+++ frontend/src/game/gamesPublics.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import tokenService from "../services/token.service";
 import jwtDecode from 'jwt-decode';
-import { Button } from 'reactstrap';
+import { Button, Table } from 'reactstrap';
 
 const jwt = tokenService.getLocalAccessToken();
 
 const GamesPublics = () => {
   const [publicGames, setPublicGames] = useState([]);
   const [message, setMessage] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
   const [game, setGame] = useState("");
-
+  
   useEffect(() => {
     fetch(
-      "/api/v1/game/publics", 
+      "/api/v1/game/publics",
       {
         method: "GET",
         headers: {
@@ -35,9 +36,38 @@ const GamesPublics = () => {
       .catch((error) => setMessage(error.message));
   }, []);
 
-  const handleJoinClick = (gameId) => {
+  useEffect(() => {
+    if (jwt) {
+      fetch(
+        `/api/v1/users/${tokenService.getUserName()}/loggedIn`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error(`Error fetching online users: ${response.status}`);
+          }
+        })
+        .then((data) => {
+          setOnlineUsers(data);
+        })
+        .catch((error) => setMessage(error.message));
+    }
+  }, [jwt]);
+
+    
+  const handleJoinClick = (game) => {
+  
     fetch(
-      "/api/v1/game/joinPublic/" + gameId,
+      "/api/v1/game/join" + (game ? "/" + game : ""),
       {
         method: "POST",
         headers: {
@@ -72,14 +102,28 @@ const GamesPublics = () => {
       <Button onClick={() => { window.location.href = '/game/'}}>Crear juego o unirse a partida privada</Button>
       {message && <p>{message}</p>}
       <ul style={{ listStyle: 'none', padding: '0' }}>
-        {publicGames
-          .map((publicGame) => (
-            <li key={publicGame.id} style={{ border: '2px solid #000', borderRadius: '8px', marginBottom: '10px', padding: '10px' }}>
-              {publicGame.name} - Jugadores: {publicGame.players.length}
-              <button onClick={() => handleJoinClick(publicGame.id)}>Join</button>
-            </li>
-          ))}
+        {publicGames.map((publicGame) => (
+          <li key={publicGame.id} style={{ border: '2px solid #000', borderRadius: '8px', marginBottom: '10px', padding: '10px' }}>
+            {publicGame.name} - Jugadores: {publicGame.players.length}
+            <button onClick={() =>  handleJoinClick(publicGame.code)}>Join</button>
+          </li>
+        ))}
       </ul>
+      <h2>Online Users</h2>
+      <Table>
+        <thead>
+          <tr>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          {onlineUsers.map((user) => (
+            <tr key={user.id}>
+              <td>{user.username}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 };
