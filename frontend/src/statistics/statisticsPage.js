685e95128173b8cbe0e2285d150b4f2a0a2cc589
fradegut <fradegut@alum.us.es>
2024-01-15 23:04:42 +0100

    achievements

--- frontend/src/statistics/statisticsPage.js
+++ frontend/src/statistics/statisticsPage.js
@@ -2,9 +2,6 @@ import React, { useState, useEffect } from "react";
 import tokenService from "../services/token.service";
 import useFetchState from "../util/useFetchState";
 
-import { Form, Button, Input, Label, Text  } from "reactstrap";
-import { Link } from "react-router-dom";
-
 const jwt = tokenService.getLocalAccessToken();
 
 export default function StatsList() {
@@ -15,208 +12,199 @@ export default function StatsList() {
     victories: 0,
     defeated: 0,
     totalMatches: 0,
-    mediaPlayersPerGame: 0
+    mediaPlayersPerGame: 0,
   });
 
   const [stats, setStats] = useFetchState([]);
+  const [achievements, setAchievements] = useFetchState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-
-        const achievementsResponse = await fetch(`/api/v1/achievements/all/${user.username}`, {
+  const fetchData = async () => {
+    try {
+      const winnedResponse = await fetch(
+        `/api/v1/achievements/allWinnedGames/${user.id}`,
+        {
           method: "GET",
           headers: {
-            "Authorization": `Bearer ${jwt}`,
+            Authorization: `Bearer ${jwt}`,
             Accept: "application/json",
             "Content-Type": "application/json",
-          }
-
-          
-        });
-
-        if (!achievementsResponse.ok) {
-          throw new Error(`HTTP error! Status: ${achievementsResponse.status}`);
+          },
         }
+      );
 
-        const achievementsData = await achievementsResponse.json();
-        console.log('Achievements Data:', achievementsData);
-
-        const winnedResponse = await fetch(`/api/v1/achievements/allWinnedGames/${user.id}`, {
+      const totalGamesPlayedResponse = await fetch(
+        `/api/v1/achievements/allPlayedGames/${user.username}`,
+        {
           method: "GET",
           headers: {
-            "Authorization": `Bearer ${jwt}`,
+            Authorization: `Bearer ${jwt}`,
             Accept: "application/json",
             "Content-Type": "application/json",
-          }
-        });
+          },
+        }
+      );
 
-        const totalGamesPlayedResponse = await fetch(`/api/v1/achievements/allPlayedGames/${user.username}`, {
+      const mediaResponse = await fetch(
+        `/api/v1/achievements/mediaPlayers/${user.username}`,
+        {
           method: "GET",
           headers: {
-            "Authorization": `Bearer ${jwt}`,
+            Authorization: `Bearer ${jwt}`,
             Accept: "application/json",
             "Content-Type": "application/json",
-          }
-        });
-
-        const mediaResponse = await fetch(`/api/v1/achievements/mediaPlayers/${user.username}`, {
-          method: "GET",
-          headers: {
-            "Authorization": `Bearer ${jwt}`,
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          }
-        });
-
-        if (!winnedResponse.ok) {
-          throw new Error(`HTTP error! Status: ${winnedResponse.status}`);
+          },
         }
+      );
 
-        if (!mediaResponse.ok) {
-          throw new Error(`HTTP error! Status: ${winnedResponse.status}`);
-        }
-
-        const winnedData = await winnedResponse.json();
-        console.log('Winned Data:', winnedData);
-
-        const gamesPlayedData = await totalGamesPlayedResponse.json();
-        console.log('Games Played Data:', gamesPlayedData);
-
-        const mediaData = await mediaResponse.json();
-        console.log('Media Data:', mediaData);
-
-        setStatistic((prevStatistic) => ({
-          ...prevStatistic,
-          victories: winnedData,
-          totalMatches: gamesPlayedData,
-          defeated: gamesPlayedData - winnedData,
-          mediaPlayersPerGame: mediaData
-        }));
-
-      setStats(achievementsData);
-
-      const statsAchievement = achievementsData.find((achievement) => achievement.description === "stats");
-
-        if (!statsAchievement) {
-          const createStatsAchievementResponse = await fetch("/api/v1/achievements", {
-            method: "POST",
-            headers: {
-              "Authorization": `Bearer ${jwt}`,
-              Accept: "application/json",
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              totalMatches: 0,
-              totalVictories: 0,
-              totalIron: 0,
-              totalGold: 0,
-              totalSteal: 0,
-              totalObjects: 0,
-              description: "stats",
-            }),
-          });
-
-          if (!createStatsAchievementResponse.ok) {
-            throw new Error(`HTTP error! Status: ${createStatsAchievementResponse.status}`);
-          }
-
-          const createdStatsAchievement = await createStatsAchievementResponse.json();
-          console.log('Created Stats Achievement:', createdStatsAchievement);
-
-          // Actualizar el estado de stats con el nuevo logro creado
-          setStats((prevStats) => [...prevStats, createdStatsAchievement]);
-        }
-        
-      } catch (error) {
-        console.error('Error in fetchData:', error);
-        alert(error.message);
+      if (!winnedResponse.ok) {
+        throw new Error(`HTTP error! Status: ${winnedResponse.status}`);
       }
-    };
-    function updateStatsAchievement(){
-      try {
-        const statsAchievement = stats.find((achievement) => achievement.description === "stats");
-  
-      if (!statsAchievement || !statsAchievement.id) {
-        console.error('Error: ID of the achievement is undefined or null');
-        return;
+
+      if (!mediaResponse.ok) {
+        throw new Error(`HTTP error! Status: ${mediaResponse.status}`);
       }
-  
-      const updateStatsAchievementResponse = fetch(`/api/v1/achievements/3`, {
-        method: "PUT",
+
+      const winnedData = await winnedResponse.json();
+      console.log("Winned Data:", winnedData);
+
+      const gamesPlayedData = await totalGamesPlayedResponse.json();
+      console.log("Games Played Data:", gamesPlayedData);
+
+      const mediaData = await mediaResponse.json();
+      console.log("Media Data:", mediaData);
+
+      setStatistic((prevStatistic) => ({
+        ...prevStatistic,
+        victories: winnedData,
+        totalMatches: gamesPlayedData,
+        defeated: gamesPlayedData - winnedData,
+        mediaPlayersPerGame: mediaData,
+      }));
+    } catch (error) {
+      console.error("Error in fetchData:", error);
+      alert(error.message);
+    }
+  };
+
+  const fetchAchievements = async () => {
+    try {
+      const achievementsResponse = await fetch("/api/v1/achievements", {
+        method: "GET",
         headers: {
-          "Authorization": `Bearer ${jwt}`,
+          Authorization: `Bearer ${jwt}`,
           Accept: "application/json",
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          id: statsAchievement.id,
-          description: "stats",
-          totalIron:0,
-          totalGold:0,
-          totalSteal:0,
-          totalObjects:0,
-          totalMatches: statistic.totalMatches,
-          totalVictories: statistic.victories,
-          totalGameTime: statistic.totalGameTime
-          // Puedes agregar más campos aquí según tus necesidades
-        }),
       });
-  
-      if (!updateStatsAchievementResponse.ok) {
-        throw new Error(`HTTP error! Status: ${updateStatsAchievementResponse.status}`);
-      }
-  
-      console.log(statsAchievement)
-  
-      } catch (error) {
-        console.error('Error updating Stats Achievement:', error);
-        alert(error.message);
-      }
-    };
 
-    updateStatsAchievement()
+      if (!achievementsResponse.ok) {
+        throw new Error(
+          `HTTP error! Status: ${achievementsResponse.status}`
+        );
+      }
+
+      const achievementsData = await achievementsResponse.json();
+      console.log("All Achievements Data:", achievementsData);
+
+      // Ensure winnedData and gamesPlayedData are valid numbers
+      const validWinnedData =
+        typeof statistic.victories === "number" ? statistic.victories : 0;
+      const validGamesPlayedData =
+        typeof statistic.totalMatches === "number" ? statistic.totalMatches : 0;
+
+      // Compare achievements with user stats
+      const updatedAchievements = achievementsData
+        .filter((achievement) => achievement.description !== "stats")
+        .map((achievement) => {
+          if (
+            typeof achievement.totalVictories === "number" &&
+            typeof achievement.totalMatches === "number"
+          ) {
+            return {
+              ...achievement,
+              achieved:
+                achievement.totalVictories <= validWinnedData &&
+                achievement.totalMatches <= validGamesPlayedData,
+            };
+          }
+          return achievement; // Skip comparison for null values
+        });
+
+      setAchievements(updatedAchievements);
+    } catch (error) {
+      console.error("Error fetching all achievements:", error);
+      alert(error.message);
+    }
+  };
+
+  useEffect(() => {
     fetchData();
-  }, []);
+    fetchAchievements();
+  }, [statistic]); // Re-run effect when statistic changes
 
-  console.log(statistic)
-
-  const filteredStats = stats.filter((achievement) => achievement.description === "stats");
-
-  const statsList = Array.isArray(filteredStats) && filteredStats.map((request, index) => (
-    <tr key={index}>
-      <td>{statistic.totalMatches}</td>
-      <td>{statistic.victories}</td>
-      <td>{statistic.defeated}</td>
-      <td>{statistic.mediaPlayersPerGame}</td>
-    </tr>
-  ));
-
+  const nonStatsAchievementsList =
+    Array.isArray(achievements) &&
+    achievements.map((achievement, index) => (
+      <li key={index}>
+        {achievement.name} - {achievement.achieved ? "Achieved" : "Not Achieved"}
+      </li>
+    ));
 
   return (
-    <div className="auth-page-container" style={{ height: "100vh", marginTop: "65px", textAlign: "center" }}>
+    <div
+      className="auth-page-container"
+      style={{ height: "100vh", marginTop: "65px", textAlign: "center" }}
+    >
       <div className="friends" style={{ marginTop: "10px" }}>
         <h3 style={{ marginTop: "30px" }}>My Stats</h3>
       </div>
       <table>
-          <thead>
-            <tr>
-              <th width="15%" className="text-center" style={{ borderBottom: "2px solid black" }}>
-                Total Matches
-              </th>
-              <th width="15%" className="text-center" style={{ borderBottom: "2px solid black" }}>
-                Total Victories
-              </th>
-              <th width="15%" className="text-center" style={{ borderBottom: "2px solid black" }}>
-                Total Defeates
-              </th>
-              <th width="15%" className="text-center" style={{ borderBottom: "2px solid black" }}>
-                Media Players Per Game
-              </th>
-            </tr>
-          </thead>
-          <tbody>{statsList[0]}</tbody>
-        </table>
+        <thead>
+          <tr>
+            <th
+              width="15%"
+              className="text-center"
+              style={{ borderBottom: "2px solid black" }}
+            >
+              Total Matches
+            </th>
+            <th
+              width="15%"
+              className="text-center"
+              style={{ borderBottom: "2px solid black" }}
+            >
+              Total Victories
+            </th>
+            <th
+              width="15%"
+              className="text-center"
+              style={{ borderBottom: "2px solid black" }}
+            >
+              Total Defeats
+            </th>
+            <th
+              width="15%"
+              className="text-center"
+              style={{ borderBottom: "2px solid black" }}
+            >
+              Media Players Per Game
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{statistic.totalMatches}</td>
+            <td>{statistic.victories}</td>
+            <td>{statistic.defeated}</td>
+            <td>{statistic.mediaPlayersPerGame}</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div>
+        <h3>Achievements</h3>
+        <ul>{nonStatsAchievementsList}</ul>
+      </div>
     </div>
   );
-};
+}
